3e396d7b8baac01a3f01ccb441facb849bffcdc7
KolmogorovMixail92 <michailkolmogorov1992@gmail.com>
2020-09-24 09:59:48 +0700

    24.09.20

---
+++ VISUAL STUDIO/Form3.cs практика 2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace практика_2_ооп2
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "пароль")
+            {
+                Form4 form4 = new Form4();
+                form4.Show();
+            }
+            else
+            {
+                MessageBox.Show("Ты чЁ !!!");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
